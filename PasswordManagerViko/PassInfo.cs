340881f54d1dd0506f195d3bc2c10520b5359d83
TruthFire <truth.fire@yandex.ru>
2022-05-03 21:45:11 +0300

    Add project files.

---
+++ PasswordManagerViko/PassInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManagerViko
+{
+    internal class PassInfo
+    {
+
+        int id;
+        string name, login, password, link, comment;
+
+    }
+}
