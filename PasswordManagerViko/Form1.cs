30409f7491a11fbcca769c0e0197e84331e683b2
TruthFire <truth.fire@yandex.ru>
2022-05-05 21:10:18 +0300

    password updating

--- PasswordManagerViko/Form1.cs
+++ PasswordManagerViko/Form1.cs
@@ -125,6 +125,12 @@ namespace PasswordManagerViko
         {
             LoadInfo(passList);
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UpdatePassword up = new UpdatePassword(passList, this);
+            up.Show();
+        }
     }
 }
 
@@ -133,7 +139,7 @@ namespace PasswordManagerViko
  Paleidus sistemą pirmą kartą sukuriamas .csv arba .txt failas. Išjungiant sistemą šis failas turi būti užšifruojamas AES algoritmu. Kitą kartą paleidus sistemą failas yra dešifruojamas. (4 taškai) done
 Naujo slaptažodžio išsaugojimas: užpildžius formą (pavadinimas, slaptažodis, URL/aplikacija, komentaras), visa jos informacija saugojama .csv arba .txt faile. Slaptažodžiui pritaikomas šifravimo algoritmas (pvz.: AES, DES ar RSA. Renkatės savo nuožiūra). (3 taškai) DONE
 Slaptažodžio paieška pagal pavadinimą. (2 taškai) DONE
-Slaptažodžio atnaujinimas pagal pavadinimą: suradus tinkamą slaptažodį jis pakeičiamas naujai įvestu. Naujam slaptažodžiui taip pat turi būti pritaikytas šifravimo algoritmas. (2 taškai) 
+Slaptažodžio atnaujinimas pagal pavadinimą: suradus tinkamą slaptažodį jis pakeičiamas naujai įvestu. Naujam slaptažodžiui taip pat turi būti pritaikytas šifravimo algoritmas. (2 taškai) DONE
 Slaptažodžio ištrynimas pagal pavadinimą: suradus tinkamą slaptažodį visa informacija apie jį ištrinama iš .csv arba .txt failo. (2 taškai)
 Paleidus sistemą pirmą kartą reikalinga vartotojo paskyros sukūrimo forma: slapyvardis, slaptažodis (šifruojamas PBKDF2, Bcrypt, Scrypt, Argon2 arba pasirenkant maišos funkciją). Kuriant vartotojo paskyrą yra sugeneruojamas ir vartotojui priskiriamas .csv arba .txt failas. Failas yra užšifruojamas AES algoritmu. (3 taškai)
 Prisijungimas prie sistemos: vartotojui prijungus failas dešifruojamas. (3 taškai)
--- PasswordManagerViko/UpdatePassword.cs
+++ PasswordManagerViko/UpdatePassword.cs
@@ -12,9 +12,41 @@ namespace PasswordManagerViko
 {
     public partial class UpdatePassword : Form
     {
-        public UpdatePassword()
+        List<PassInfo> passList = new List<PassInfo>();
+        Form1 currForm;
+        public UpdatePassword(List<PassInfo> pList, Form1 form)
         {
+            passList = pList;
+            currForm = form; 
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = passList.Where(x => x.name.Contains(textBox1.Text)).ToList();
+            // MessageBox.Show(a.ToString());
+            string encPass = AESHelper.AES_EncryptString(textBox2.Text);
+            if (a.Count() != 0)
+            {
+                foreach(PassInfo p in passList)
+                {
+                    if(p.name.Contains(textBox1.Text))
+                    {
+                        p.password = encPass;
+                    }
+                }
+                currForm.passList = this.passList;
+                currForm.LoadInfo(currForm.passList);
+
+                MessageBox.Show("UPDATED");
+
+
+            }
+            else
+            {
+                MessageBox.Show("Nothing found!");
+            }
         }
     }
 }
